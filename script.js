7d8ba23630a3d9c5cb989d3500784b862e10e423
Clayton Batiquin <cbatiquin.dev@gmail.com>
2022-03-24 20:21:46 -0500

    function to play a round
    started with basic algorithm and started on syntax BUT I'm regretting the if/else, thinking I should've gone the switch route

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // function to serve as the computer player
 // using random number choice and assigning ranges to choices
-let computerPlay = () => {
+const computerPlay = () => {
     let choice = Math.floor(Math.random()*30) + 1;
     // numbers 1-10 are ROCK
     // numbers 21-30 are PAPER
@@ -14,6 +14,17 @@ let computerPlay = () => {
 
 }
 
-let test = computerPlay();
-
-console.log(test);
+// function to play a round of the game
+// two params req'd: playerSelection and computerSelection
+const playRound = (playerSelection = "ROCK", computerSelection = "SCISSORS") => {
+    // need select from player
+    let player = prompt("Rock, paper, or scissors?", "RoShamBo");
+    player.toUpperCase();
+    // need select from computer
+    let computer = computerPlay();
+    // compare answers
+    // if rock beats scissors
+    // if scissors beats paper
+    // if paper beats rock
+    return player;
+}
