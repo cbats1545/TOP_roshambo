bb89189f5fb80e0b42a37779585631f1e8241121
Clayton Batiquin <cbatiquin.dev@gmail.com>
2022-03-26 13:23:28 -0500

    Update script.js
    added a game function that loops the playRound five times, currently trying to figure out how to get an ending results (victory/defeat)

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // function to serve as the computer player
 // using random number choice and assigning ranges to choice
 
+let victory;
+let defeat;
+
 const computerPlay = () => {
     let choice = Math.floor(Math.random()*30) + 1;
     if (choice <= 10) {
@@ -18,19 +21,25 @@ const playRound = (playerSelection, computerSelection) => {
     let player = prompt("Rock, paper, or scissors?");
     playerSelection = player.toUpperCase();
     let computer = computerPlay();
-    computerSelection = computer.toUpperCase();
+    computerSelection = computer.toUpperCase(); 
     console.log(`Computer plays: ${computerSelection}`);
     if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+            victory++;
             return "You win. Paper covers rock.";
     } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+            victory++;
             return "You win. Scissors cut paper.";
-    } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK"){
+    } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+            victory++;
             return "You win. Rock breaks scissors.";
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+            defeat++;
             return "You lose. Paper covers rock.";
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+            defeat++;
             return "You lose. Scissors cut paper.";
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+            defeat++;
             return "You lose. Rock breaks scissors.";
     }
     return "It's a tie!";
@@ -38,5 +47,24 @@ const playRound = (playerSelection, computerSelection) => {
     
 
 // Don't forgot to call the function!!
-let results = playRound();
-console.log(results);
+
+
+const game = () => {
+    for (i = 0; i < 5; i++) {
+        console.log(playRound());
+    }
+
+}
+
+game();
+
+const results = () => {
+    if (victory >= 3) {
+        return `You win!!!`;
+    } else if (defeat >= 3) {
+        return `You lose...`;
+    }
+}
+
+let ending = results();
+console.log(ending);
