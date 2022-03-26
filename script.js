5b9a29d3ab305e46c4d337204bc0d66a809a00c8
Clayton Batiquin <cbatiquin.dev@gmail.com>
2022-03-26 12:42:20 -0500

    switch statement
    tried out using a switch statement but I don't think I understand it well enough OR this isn't the correct solution for what i'm trying to do

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // function to serve as the computer player
 // using random number choice and assigning ranges to choice
+
 const computerPlay = () => {
     let choice = Math.floor(Math.random()*30) + 1;
     if (choice <= 10) {
@@ -8,38 +9,41 @@ const computerPlay = () => {
         return "PAPER";
     } 
     return "SCISSORS";
+}
 
-}
-/*
-// function to play a round of the game
-// two params req'd: playerSelection and computerSelection
-const playRound = (playerSelection = "ROCK", computerSelection = "SCISSORS") => {
-    // need select from player
-    let player = prompt("Rock, paper, or scissors?", "RoShamBo");
-    player.toUpperCase();
-    // need select from computer
-    let computer = computerPlay();
-    // compare answers
-    // if rock beats scissors
-    if (player === "ROCK" && computer === "SCISSORS") {
-        return "You win. Rock smashes scissors.";
-    } else if (player === "SCISSORS" && computer === "PAPER") {
-        // if scissors beats paper
-        return "You win. Scissors cut through paper.";
-    } else if (player === "PAPER" && computer === "ROCK") {
-        // if paper beats rock
-        return "You win. "
-    }
-    
-}
-*/
+// function to play single round of roshambo
+// two param req'd: one for playerSelection and one for computerSelection
+
 const playRound = (playerSelection, computerSelection) => {
     let player = prompt("Rock, paper, or scissors?");
     playerSelection = player.toUpperCase();
     let computer = computerPlay();
     computerSelection = computer.toUpperCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
+    let results;
+    switch (results) {
+        case (computerSelection === "ROCK" && playerSelection === "PAPER"):
+            return "You win. Paper covers rock.";
+            break;
+        case (computerSelection === "PAPER" && playerSelection === "SCISSORS"):
+            return "You win. Scissors cut paper.";
+            break;
+        case (computerSelection === "SCISSORS" && playerSelection === "ROCK"):
+            return "You win. Rock breaks scissors.";
+            break;
+        case (playerSelection === "ROCK" && computerSelection === "PAPER"):
+            return "You lose. Paper covers rock.";
+            break;
+        case (playerSelection === "PAPER" && computerSelection === "SCISSORS"):
+            return "You lose. Scissors cut paper.";
+            break;
+        case (playerSelection === "SCISSORS" && computerSelection === "ROCK"):
+            return "You lose. Rock breaks scissors.";
+            break;
+        default: 
+            return "It's a tie!";
+    }
+    console.log(results);
 }
 
+// Don't forgot to call the function!!
 playRound();
